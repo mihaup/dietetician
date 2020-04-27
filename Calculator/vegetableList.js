f57d74eb198dcfb7a003e5b318791881d855bebc
Michal Papmel <papmel.papmel@gmail.com>
2020-04-27 22:45:14 +0100

    updt variables

---
+++ Calculator/vegetableList.js
@@ -0,0 +1,84 @@
+
+const months = {
+    
+    january: "January",
+    february: "February",
+    march: "March",
+    april: "April",
+    may: "May",
+    june: "June",
+    july: "July",
+    august: "August",
+    september: "September",
+    october: "October",
+    november: "November",
+    december: "December"
+
+
+
+};
+
+
+
+
+const asparagus = {
+    name: "Asparagus",
+    season: [months.april, months.may, months.june]
+    
+
+ };
+
+ const aubergine = {
+    name: "Aubergine",
+    season: [months.june, months.july, months.august, months.september, months.october]
+    
+
+ };
+
+
+ const beetroot = {
+    name: "Beetroot",
+    season: [months.january, months.february, months.june, months.july, months.august, months.september, months.october, months.november, months.december]
+    
+
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const vegetables = {asparagus, aubergine, beetroot};
+
+
+
+
+
--- Calculator/vegetables.js
+++ Calculator/vegetables.js
@@ -7,7 +7,9 @@ window.onload = function(){
 
 const btnAsparagus = document.querySelector("#asparagus-button");
 const btnAubergine = document.querySelector("#aubergine-button");
+const btnBeetroot = document.querySelector("#beetroot-button");
 const content = document.querySelector(".content");
+
 
 
 
@@ -15,6 +17,8 @@ const content = document.querySelector(".content");
 
 btnAsparagus.addEventListener("click", addMe,true);
 btnAubergine.addEventListener("click",addMe,true);
+btnBeetroot.addEventListener("click", addMe, true);
+
 
 
 
@@ -28,16 +32,25 @@ function addMe(e){
     const target = e.target;
 
     const newElement = document.createElement("p");
-    const remove = document.createElement("i");
+    const removeBtn = document.createElement("i");
     target.classList.add("choosen");
     const element = document.querySelector(".choosen").parentElement;
+
+
+  
 
     var c = element.childNodes;
 
     c[0].checked = true;
     c[0].classList.add("no-events");
 
-    console.log(c[2]);
+
+
+    
+
+    
+    
+
 
     if (c[0].checked = true)
         
@@ -46,24 +59,120 @@ function addMe(e){
         newElement.innerHTML = c[0].value;
         c[2].removeEventListener("click",addMe,true);
 
+       
+
+    }
+
+    const asparagusSeasonLength = vegetables.asparagus.season.length;
+    const aubergineSeasonLength = vegetables.aubergine.season.length;
+    const beetrootSeasonLength = vegetables.beetroot.season.length;
+    
+
+
+ 
+
+
+    const w = newElement.childNodes;
+    console.log(w);
+
+
+
+    console.log(c[0].value);
+    const vegName = c[0].value;
+    const vegNameLower = vegName.toLowerCase();
+
+    
+
+    if (vegNameLower === "asparagus"){
+
+
+    for(i=0;i<asparagusSeasonLength;i++)
+    {
+
+    var div = document.createElement('span');
+    div.classList.add("padding");
+    newElement.appendChild(div);
+
+    
+        
+    }
+
+        w[1].innerHTML = vegetables.asparagus.season[0];
+        w[2].innerHTML = vegetables.asparagus.season[1];
+        w[3].innerHTML = vegetables.asparagus.season[2];
+
+        
+
+
+
+    }
+ 
+    if (vegNameLower === "aubergine"){
+
+
+        for(i=0;i<aubergineSeasonLength;i++)
+        {
+    
+        var div = document.createElement('span');
+        div.classList.add("padding");
+        newElement.appendChild(div);
+    
+        
+            
+        }
+
+        w[1].innerHTML = vegetables.aubergine.season[0];
+        w[2].innerHTML = vegetables.aubergine.season[1];
+        w[3].innerHTML = vegetables.aubergine.season[2];
+        w[4].innerHTML = vegetables.aubergine.season[3];
+        w[5].innerHTML = vegetables.aubergine.season[4];
+
     }
 
 
 
-  
     
+    if (vegNameLower === "beetroot"){
+
+
+        for(i=0;i<beetrootSeasonLength;i++)
+        {
+    
+        var div = document.createElement('span');
+        div.classList.add("padding");
+        newElement.appendChild(div);
+    
+        
+            
+        }
+
+        w[1].innerHTML = vegetables.beetroot.season[0];
+        w[2].innerHTML = vegetables.beetroot.season[1];
+        w[3].innerHTML = vegetables.beetroot.season[2];
+        w[4].innerHTML = vegetables.beetroot.season[3];
+        w[5].innerHTML = vegetables.beetroot.season[4];
+        w[6].innerHTML = vegetables.beetroot.season[5];
+        
+
+    }
+ 
+
    
+    
+
+    
 
 
-    remove.innerHTML = "-";
-    remove.classList.add("margin");
 
-    newElement.appendChild(remove);
+    removeBtn.innerHTML = "-";
+    removeBtn.classList.add("margin");
+
+    newElement.appendChild(removeBtn);
 
     content.appendChild(newElement);
 
 
-    remove.addEventListener("click", remover);
+    removeBtn.addEventListener("click", remover);
     
 
     
